eecbf9ca5b7ffc854e0a606a2b16ce60da10582a
Suei43 <sueiraphael@gmail.com>
2022-10-10 08:29:31 -0700

    edited log message

--- server.js
+++ server.js
@@ -28,5 +28,5 @@ const server = http.createServer((request,response)=>{
 const port = 3000
 
 server.listen(port,()=>{
-    console.log('Server is now running hehehe')
+    console.log('Server is now running!')
 });
